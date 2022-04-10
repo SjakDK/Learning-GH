bc3f380b305e41a9b2ae0dfd11668d382a26baf4
Morten Sundby <morten@vidcom.dk>
2022-04-10 11:16:38 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Morten Sundby on 10/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
